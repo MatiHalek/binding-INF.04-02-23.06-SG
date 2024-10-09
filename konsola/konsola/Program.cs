833db8c6c4e92b5b6adf896fb2a8beab75f166cf
Mateusz Marmuźniak <mateusz.marmuzniak.poland@gmail.com>
2024-10-09 10:17:55 +0200

    Add sorting array function in console app

--- konsola/konsola/Program.cs
+++ konsola/konsola/Program.cs
@@ -2,6 +2,21 @@
 {
     internal class Program
     {
+        static void SortArray(uint[] arrayToSort)
+        {
+            for(uint i = (uint)arrayToSort.Length - 1; i > 0; i--)
+            {
+                for (uint j = 0; j < i; j++)
+                {
+                    if (arrayToSort[j] > arrayToSort[j + 1])
+                    {
+                        uint tmp = arrayToSort[j];
+                        arrayToSort[j] = arrayToSort[j + 1];
+                        arrayToSort[j + 1] = tmp;
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
             uint[] randomNumbersArray = new uint[100];
